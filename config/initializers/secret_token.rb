c06a03698a22fec553815017e9cfd908e73e64c7
EOengineer <EOengineer@gmail.com>
2014-04-10 13:38:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GearXchanger::Application.config.secret_key_base = '6075ac947b04d7662f9f1f05d922720bebcd80e2a32d71a5a1c2c4cd5666c5fd6b49f453d968a3e9ee3b973c894f041550e36f2e639f8673d073d6676f75874c'
