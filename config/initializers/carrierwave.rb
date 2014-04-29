fa75e1cfe89be908ee204dd545fe6e5620c47932
EOengineer <EOengineer@gmail.com>
2014-04-29 16:02:29 -0400

    fog/aws region config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,7 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['gear_bucket'],
     aws_secret_access_key: ENV['gear_secret_key']
+    region: 'eu-west-1'
   }
   config.fog_directory  = "gear-#{Rails.env}"
 end
