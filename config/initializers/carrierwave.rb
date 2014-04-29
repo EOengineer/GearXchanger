0885ec7c55fa80b62ab91390603fbefc189260fb
EOengineer <EOengineer@gmail.com>
2014-04-29 17:53:01 -0400

    still working on s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['gear_bucket'],
     aws_secret_access_key: ENV['gear_secret_key']
-    region: 'eu-west-1'
   }
   config.fog_directory  = "gear-#{Rails.env}"
 end
